37e6b67479a660630ed7e081088795cdf4a05fc0
W. Michael Petullo <mike@flyn.org>
2015-08-07 14:39:47 -0400

    tid -> pid in process.h
    Signed-off-by: W. Michael Petullo <mike@flyn.org>

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -23,8 +23,8 @@ struct pcb
 
 void process_init (void);
 void init_process (struct pcb *pcb, struct thread *thread);
-tid_t process_execute (const char *file_name, char **argv);
-int process_wait (tid_t);
+pid_t process_execute (const char *file_name, char **argv);
+int process_wait (pid_t);
 void process_exit (void);
 void process_notify_parent (void);
 void process_activate (void);
