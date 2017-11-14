6bf294e26f8dc3e1a04ceaf94b94a0e31e00f8d5
Mason Adam <mason.adam@usma.edu>
2017-11-14 03:29:02 -0500

    DONE SCRIPTING TESTS

---
+++ testData/parseFiles.py
@@ -0,0 +1,34 @@
+import csv
+
+names = ["CPUNiceSet","CPUNoNice","CPUvIO","IONiceSet","IONoNice"]
+
+def parse(name):
+	file = open(name+".txt","r")
+	new = open(name+"NEW.txt","w+")
+
+	for lines in file:
+		if lines[0] == '*':
+			new.write(lines)
+
+	file.close()
+	new.close()
+
+def makeCleanData(name): #don't put .txt after name on invocation
+	file = open(name+".txt","r")
+	new = open(name+".csv","w+")
+	new.write("TID, Nice, Tick\n")
+	for lines in file:
+		a,TID,c,Nice,e,Tick = lines.split(" ")
+		
+		TID = TID[:-1]
+		Nice = Nice[:-1]
+
+		new.write(TID+", "+Nice+", "+Tick)
+
+	file.close()
+	new.close()
+
+def doAll():
+	for x in names:
+		parse(x)
+		makeCleanData(x+"NEW")
